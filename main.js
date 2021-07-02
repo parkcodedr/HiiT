d43dba47e7a0d9a6a9e9c1b235cff83a5ce5f7ad
parkcodedr <okwori32@gmail.com>
2021-07-02 12:34:02 +0100

    lesson two updated

--- main.js
+++ main.js
@@ -47,11 +47,71 @@ const bi = "5";
 console.log(bi + 5);
 console.log(Number(bi) + 5);
 
+//lesson two
+data = { "fname": "jhon" }
+const newdata = data;
+newdata.fname = "jonah"
+console.log(data);
+console.log(newdata);
+
+const score = 90;
+let newScore = score;
+newScore = 95
+console.log(score);
+console.log(newScore);
+
+const gretting = "hello mr john , you are welcome to HIIT";
+const words = gretting.split(",");
+console.log(gretting)
+console.log(words);
+const subgreeting = gretting.slice(1, 5);
+console.log(subgreeting);
+console.log(gretting.length);
+const pos = gretting.indexOf("john");
+console.log(pos);
+const isInWord = gretting.includes("hello");
+console.log(isInWord);
+
+const upperleter = gretting.toUpperCase();
+console.log(upperleter);
+
+const smallWord = gretting.substring(0, 9);
+console.log(smallWord)
+const smallWordsub = gretting.substring(0, 4);
+console.log(smallWordsub)
+console.log(gretting.replace("john", "wyldpych"));
+
+//functions
+function sayHi() {
+    return (90 + 20);
+    // console.log("hi function");
+}
+const d = sayHi();
+console.log(d);
+
+
+function login(username, password) {
+    let access;
+    if (username == "admin" && password == "admin") {
+        access = true;
+    } else {
+        access = false;
+    }
+    return access;
+}
+console.log(login("admin", "admin"));
+
+function square(num) {
+    return num * num;
+}
+function printhi() {
+    console.log("hi")
+}
+console.log(square(10));
 
 const getElement = (data) => {
     return data[0];
 }
-
 console.log(getElement(["php", "python", "javascript", "java"]));
 
 
